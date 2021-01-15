c1d03eef6a2c32791354dc2255364513bbaa44db
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 01:27:57 +0530

    ADDED: Create Board function

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,4 +1,16 @@
-
 public class TicTacToeGame {
+	
+	public static char[] createBoard() {
+		char[] board = new char[10];
+		for(int i=0;i<9;i++) {
+			board[i]= '-';
+		}
+		return board;	
+	}
+	
+	public static void main(String[] args) {
+		char[] board = createBoard();
+		
+	}
 
 }
