e1df594f34b03477d1bb880acb67446b1d3ae6f8
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 01:59:32 +0530

    ADDED: showBoard method

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -5,13 +5,21 @@ public class TicTacToeGame {
 	
 	public static char playerSymbol;
 	public static char cpuSymbol;
-	
+	static char[] board = new char[10];
 	public static char[] createBoard() {
-		char[] board = new char[10];
+
 		for(int i=0;i<9;i++) {
 			board[i]= '-';
 		}
 		return board;	
+	}
+	
+	public static void showBoard() {
+		System.out.println("**Play Board Display**\n");
+		System.out.println(" | "+board[0]+" "+board[1]+" "+board[2]+" |\n");
+		System.out.println(" | "+board[3]+" "+board[4]+" "+board[5]+" |\n");
+		System.out.println(" | "+board[6]+" "+board[7]+" "+board[8]+" |\n");
+
 	}
 	
 	public static void main(String[] args) {
@@ -23,21 +31,22 @@ public class TicTacToeGame {
 			case 1 : {
 				playerSymbol='X';
 				cpuSymbol='O';
+				System.out.println("\n\nPlayerOne Choose : X\nCPU Choose : O\n\n");
 				break;
 			}
 			case 2 :{
 				playerSymbol='O';
 				cpuSymbol='X';
+				System.out.println("\n\nPlayerOne Choose : O\nCPU Choose : X\n\n");
 				break;
 			}
 			default: {
 				System.out.println("Invalid Choice . Retry Again!");
 				return;
 			}
-			
 		}
+		
+		TicTacToeGame.showBoard();
+		
 	}
-
-
-
 }
