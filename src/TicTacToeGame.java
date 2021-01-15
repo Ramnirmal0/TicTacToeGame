2630847f4a692392c5e4a9e137c30057685e3135
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 02:14:51 +0530

    ADDED : Movability in Board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -6,6 +6,10 @@ public class TicTacToeGame {
 	public static char playerSymbol;
 	public static char cpuSymbol;
 	static char[] board = new char[10];
+	public static int playerPos;
+	public static int cpuPos;
+	
+	
 	public static char[] createBoard() {
 
 		for(int i=0;i<9;i++) {
@@ -44,6 +48,24 @@ public class TicTacToeGame {
 				System.out.println("Invalid Choice . Retry Again!");
 				return;
 			}
+		}
+		
+		TicTacToeGame.showBoard();
+		
+		System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
+		playerPos= scan.nextInt();
+		
+		switch(playerPos) {
+		case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
+		case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
+		case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
+		case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
+		case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
+		case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
+		case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
+		case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
+		case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
+		default : System.out.println("Invalid Position ! Restart game"); return;
 		}
 		
 		TicTacToeGame.showBoard();
