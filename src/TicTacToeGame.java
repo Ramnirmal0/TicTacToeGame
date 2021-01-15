795322e677c5318a5c4600af090b354a5ed63e23
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 03:07:58 +0530

    ADDED : CoinToss Feature

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -53,47 +53,98 @@ public class TicTacToeGame {
 		
 		TicTacToeGame.showBoard();
 		
-		System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
-		playerPos= scan.nextInt();
+		int myToss;
+		int tossWon;
+		System.out.println("Choose\n 0. Heads\n 1. Tails\nEnter you choice [0-1] : ");
+		myToss=scan.nextInt();
+		Random rand1 = new Random();
+		tossWon = rand1.nextInt(2);
 		
-		switch(playerPos) {
-		case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
-		case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
-		case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
-		case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
-		case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
-		case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
-		case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
-		case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
-		case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
-		default : System.out.println("Invalid Position ! Restart game"); return;
-		}
-		
-		TicTacToeGame.showBoard();
-		
-		boolean flag=false;
-		while(true) {
-			Random rand = new Random();
-			int cpuPos = rand.nextInt(9);
-			switch(cpuPos) {
-			case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
-			case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
-			case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
-			case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
-			case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
-			case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
-			case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
-			case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
-			case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
-			}
-			if(flag==true) {
-				break;
+		if(tossWon==myToss) {
+			System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
+			playerPos= scan.nextInt();
+			
+			switch(playerPos) {
+			case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
+			case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
+			case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
+			case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
+			case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
+			case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
+			case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
+			case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
+			case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
+			default : System.out.println("Invalid Position ! Restart game"); return;
 			}
 			
+			TicTacToeGame.showBoard();
+			
+			boolean flag=false;
+			while(true) {
+				Random rand = new Random();
+				int cpuPos = rand.nextInt(9);
+				switch(cpuPos) {
+				case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
+				case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
+				case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
+				case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
+				case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
+				case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
+				case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
+				case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
+				case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
+				}
+				if(flag==true) {
+					break;
+				}
+				
+			}
+			TicTacToeGame.showBoard();
 		}
-		TicTacToeGame.showBoard();
 		
-		
-		
+		else {
+			
+			boolean flag=false;
+			while(true) {
+				Random rand = new Random();
+				int cpuPos = rand.nextInt(9);
+				switch(cpuPos) {
+				case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
+				case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
+				case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
+				case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
+				case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
+				case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
+				case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
+				case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
+				case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
+				}
+				if(flag==true) {
+					break;
+				}
+				
+			}
+			TicTacToeGame.showBoard();
+			
+			System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
+			playerPos= scan.nextInt();
+			
+			switch(playerPos) {
+			case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
+			case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
+			case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
+			case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
+			case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
+			case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
+			case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
+			case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
+			case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
+			default : System.out.println("Invalid Position ! Restart game"); return;
+			}
+			
+			TicTacToeGame.showBoard();
+			
+		}
+
 	}
 }
