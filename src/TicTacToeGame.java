f2e5c9544f1b5e94d3b00ed173181f563b87ebea
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 01:45:26 +0530

    ADDED : Choose letter features

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,4 +1,10 @@
+import java.io.*;
+import java.util.*;
+
 public class TicTacToeGame {
+	
+	public static char playerSymbol;
+	public static char cpuSymbol;
 	
 	public static char[] createBoard() {
 		char[] board = new char[10];
@@ -10,7 +16,28 @@ public class TicTacToeGame {
 	
 	public static void main(String[] args) {
 		char[] board = createBoard();
-		
+		System.out.println("Select the Letter you wanted \n  press 1 . Choose X \n  Press 2 . Choose O\n\nEnter your Choice : ");
+		Scanner scan =new Scanner(System.in);
+		int choice = scan.nextInt();
+		switch(choice){
+			case 1 : {
+				playerSymbol='X';
+				cpuSymbol='O';
+				break;
+			}
+			case 2 :{
+				playerSymbol='O';
+				cpuSymbol='X';
+				break;
+			}
+			default: {
+				System.out.println("Invalid Choice . Retry Again!");
+				return;
+			}
+			
+		}
 	}
+
+
 
 }
