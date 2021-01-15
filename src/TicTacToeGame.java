0aa8dfd4382bf0db9f67f86e8159efd7a5d0f37d
Nirmal-Ram <nirmalramofficial@gmail.com>
2021-01-16 04:41:04 +0530

    ADDED : Winners algorithm

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -8,6 +8,7 @@ public class TicTacToeGame {
 	static char[] board = new char[10];
 	public static int playerPos;
 	public static int cpuPos;
+	public static char exitCode='0';
 	
 	
 	
@@ -25,6 +26,116 @@ public class TicTacToeGame {
 		System.out.println(" | "+board[3]+" "+board[4]+" "+board[5]+" |\n");
 		System.out.println(" | "+board[6]+" "+board[7]+" "+board[8]+" |\n");
 
+	}
+	
+	public static void winnerStatus(char symbol) {
+		
+		if(board[0]!='-' && board[0]==board[1] && board[1]==board[2]) {
+			if(board[0]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+
+		}
+		
+		
+		else if(board[3]!='-' && board[3]==board[4] && board[4]==board[5]) {
+				if(board[3]==symbol) {
+					System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+					
+				}
+				else {
+					System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+					
+				}
+				exitCode='1';
+			}
+		
+		
+		else if(board[7]!='-' && board[6]==board[7] && board[7]==board[8]) {
+			if(board[7]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
+		else if(board[6]!='-' && board[0]==board[3] && board[3]==board[6]) {
+			if(board[6]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
+		else if(board[1]!='-' && board[1]==board[4] && board[4]==board[7]) {
+			if(board[1]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
+		else if(board[5]!='-' && board[2]==board[5] && board[5]==board[8]) {
+			if(board[5]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
+		else if(board[4]!='-' && board[0]==board[4] && board[4]==board[8]) {
+			if(board[4]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
+		else if(board[2]!='-' && board[2]==board[4] && board[4]==board[6]) {
+			if(board[2]==symbol) {
+				System.out.println("\n*******  PLAYER ONE WINS THE GAME  *********\n");
+				
+			}
+			else {
+				System.out.println("\n*******  CPU ONE WINS THE GAME  *********\n");
+				
+			}
+			exitCode='1';
+		}
+		
+		
 	}
 	
 	public static void main(String[] args) {
@@ -61,89 +172,122 @@ public class TicTacToeGame {
 		tossWon = rand1.nextInt(2);
 		
 		if(tossWon==myToss) {
-			System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
-			playerPos= scan.nextInt();
 			
-			switch(playerPos) {
-			case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
-			case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
-			case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
-			case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
-			case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
-			case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
-			case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
-			case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
-			case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
-			default : System.out.println("Invalid Position ! Restart game"); return;
-			}
 			
-			TicTacToeGame.showBoard();
-			
-			boolean flag=false;
 			while(true) {
-				Random rand = new Random();
-				int cpuPos = rand.nextInt(9);
-				switch(cpuPos) {
-				case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
-				case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
-				case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
-				case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
-				case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
-				case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
-				case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
-				case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
-				case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
+			
+				System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
+				playerPos= scan.nextInt();
+				
+				switch(playerPos) {
+				case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
+				case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
+				case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
+				case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
+				case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
+				case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
+				case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
+				case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
+				case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
+				default : System.out.println("Invalid Position ! Restart game"); return;
 				}
-				if(flag==true) {
+				
+				TicTacToeGame.showBoard();
+				
+				TicTacToeGame.winnerStatus(playerSymbol);
+				
+				if(exitCode==1) {
 					break;
 				}
 				
+				boolean flag=false;
+				while(true) {
+					Random rand = new Random();
+					int cpuPos = rand.nextInt(9);
+					switch(cpuPos) {
+					case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
+					case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
+					case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
+					case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
+					case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
+					case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
+					case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
+					case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
+					case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
+					}
+					if(flag==true) {
+						break;
+					}
+					
+				}
+				TicTacToeGame.showBoard();
+				
+				TicTacToeGame.winnerStatus(cpuSymbol);
+				
+				if(exitCode==1) {
+					break;
+				}
+			
 			}
-			TicTacToeGame.showBoard();
+
 		}
 		
 		else {
 			
-			boolean flag=false;
-			while(true) {
-				Random rand = new Random();
-				int cpuPos = rand.nextInt(9);
-				switch(cpuPos) {
-				case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
-				case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
-				case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
-				case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
-				case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
-				case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
-				case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
-				case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
-				case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
-				}
-				if(flag==true) {
-					break;
-				}
-				
+			while(true){
+			
+					boolean flag=false;
+					while(true) {
+						Random rand = new Random();
+						int cpuPos = rand.nextInt(9);
+						switch(cpuPos) {
+						case 0 : {if(board[0]=='-') { board[0]=cpuSymbol; flag=true;} break;}
+						case 1 : {if(board[1]=='-') { board[1]=cpuSymbol; flag=true;} break;}
+						case 2 : {if(board[2]=='-') { board[2]=cpuSymbol; flag=true;} break;}
+						case 3 : {if(board[3]=='-') { board[3]=cpuSymbol; flag=true;} break;}
+						case 4 : {if(board[4]=='-') { board[4]=cpuSymbol; flag=true;} break;}
+						case 5 : {if(board[5]=='-') { board[5]=cpuSymbol; flag=true;} break;}
+						case 6 : {if(board[6]=='-') { board[6]=cpuSymbol; flag=true;} break;}
+						case 7 : {if(board[7]=='-') { board[7]=cpuSymbol; flag=true;} break;}
+						case 8 : {if(board[8]=='-') { board[8]=cpuSymbol; flag=true;} break;}
+						}
+						if(flag==true) {
+							break;
+						}
+						
+					}
+					TicTacToeGame.showBoard();
+					TicTacToeGame.winnerStatus(cpuSymbol);
+					
+					if(exitCode==1) {
+						break;
+					}
+					
+					System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
+					playerPos= scan.nextInt();
+					
+					switch(playerPos) {
+					case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
+					case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
+					case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
+					case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
+					case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
+					case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
+					case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
+					case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
+					case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
+					default : System.out.println("Invalid Position ! Restart game"); return;
+					}
+					
+					TicTacToeGame.showBoard();
+					TicTacToeGame.winnerStatus(playerSymbol);
+					
+					if(exitCode==1) {
+						break;
+					}
+			
 			}
-			TicTacToeGame.showBoard();
-			
-			System.out.println("\nSelect the position to in board from range [ 0 - 8 ] :");
-			playerPos= scan.nextInt();
-			
-			switch(playerPos) {
-			case 0 : {if(board[0]=='-') { board[0]=playerSymbol;} break;}
-			case 1 : {if(board[1]=='-') { board[1]=playerSymbol;} break;}
-			case 2 : {if(board[2]=='-') { board[2]=playerSymbol;} break;}
-			case 3 : {if(board[3]=='-') { board[3]=playerSymbol;} break;}
-			case 4 : {if(board[4]=='-') { board[4]=playerSymbol;} break;}
-			case 5 : {if(board[5]=='-') { board[5]=playerSymbol;} break;}
-			case 6 : {if(board[6]=='-') { board[6]=playerSymbol;} break;}
-			case 7 : {if(board[7]=='-') { board[7]=playerSymbol;} break;}
-			case 8 : {if(board[8]=='-') { board[8]=playerSymbol;} break;}
-			default : System.out.println("Invalid Position ! Restart game"); return;
-			}
-			
-			TicTacToeGame.showBoard();
-			
+		
 		}
 
 	}
